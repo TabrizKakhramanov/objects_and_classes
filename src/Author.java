09c36bafeb63a91e1edcce8dd4e06e1c9af93e67
TabrizKakhramanov <tabriz9998@gmil.com>
2024-10-31 13:28:11 +0300

    Made homework

---
+++ src/Author.java
@@ -0,0 +1,20 @@
+public class Author {
+    private String firstName;
+    private String secondName;
+
+    public Author (String firstName, String secondName){
+        this.firstName=firstName;
+        this.secondName=secondName;
+    }
+    public String getFirstName(){
+        return this.firstName;
+    }
+    public String getSecondName(){
+        return this.secondName;
+    }
+
+    @Override
+    public String toString() {
+        return getFirstName()+" "+getSecondName();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import java.util.concurrent.atomic.AtomicStampedReference;
 
 public class Main {
     public static void main(String[] args) {
+        Author firstAuthor = new Author("Лев", "Толстой");
+        Author secondAuthor = new Author("Сергей", "Пушкин");
+        Book firstBook = new Book("Война и Мир", firstAuthor, 2008);
+        Book secondBook = new Book("Евгений Онегин", secondAuthor, 2010);
 
+        System.out.println(firstBook.toString());
+        firstBook.setPublicationYear(2024);
+
+
+        System.out.println(firstBook.toString());
+        System.out.println(secondBook.toString());
     }
 }
